5a6ee569d80001be5661a7a8a3dfbd396f3fb6bf
rizuan31 <55524590+rizuan31@users.noreply.github.com>
2021-06-05 22:51:56 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/fragmentz/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.fragmentz
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            val message = "hi, this a message"
+            val sendTextIntent = Intent(Intent.ACTION_SEND)
+            sendTextIntent.type = "text/plain"
+            sendTextIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(sendTextIntent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
